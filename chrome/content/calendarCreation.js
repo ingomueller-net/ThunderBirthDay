d6a695514c168a4519e379c2c403bc64102ec3f9
ingo <ingo@c6a9ee40-8b02-4321-a5a2-18dd8e6a8d40>
2008-12-22 21:21:08 +0000

    IMPORT: creation dialog ready & names adopted.
    git-svn-id: file:///tmp/tbd-svn-repo/trunk@3 c6a9ee40-8b02-4321-a5a2-18dd8e6a8d40

--- chrome/content/calendarCreation.js
+++ chrome/content/calendarCreation.js
@@ -1,28 +1,28 @@
 /**
- * Modifies the calendarCreation dialog to the suits of the TestExt
+ * Modifies the calendarCreation dialog to the suits of ThunderBirthDays
  */
-function loadTestExt() {
-	// Show locationPageLocal instead of skipping the location page for local calendars
+function cTBD_loadCalendarCreation() {
+	// Show cTBD_locationPageLocal instead of skipping the location page for local calendars
 	var initialPage = document.getElementsByAttribute('pageid', 'initialPage')[0];
-	initialPage.setAttribute('onpageadvanced','onInitialAdvanceTestExt()');
+	initialPage.setAttribute('onpageadvanced','cTBD_onInitialAdvance()');
 	
 	// Enable automatic name proposition for the new calender when choosing thunderbird format
 	var  customizePage = document.getElementsByAttribute('pageid', 'customizePage')[0];
 	customizePage.setAttribute('onpageshow',
-				"initNameLocal(); " + customizePage.getAttribute('onpageshow'));
+				"cTBD_initName(); " + customizePage.getAttribute('onpageshow'));
 
 	customizePage.setAttribute('onpageadvanced',
-				 "preCreateCalendar(); " + customizePage.getAttribute('onpageadvanced'));
+				 "cTBD_preCreateCalendar(); " + customizePage.getAttribute('onpageadvanced'));
 }
 
 /**
  * Replaces the onInitialAdvance() function. Changes the next page after the initial page according to the selected radio button.
  */
-function onInitialAdvanceTestExt() {
+function cTBD_onInitialAdvance() {
     var type = document.getElementById('calendar-type').selectedItem.value;
     var page = document.getElementsByAttribute('pageid', 'initialPage')[0];
     if (type == 'local')
-        page.next = 'locationPageLocal';
+        page.next = 'cTBD_locationPageLocal';
     else
         page.next = 'locationPage';
 }
@@ -30,26 +30,26 @@ function onInitialAdvanceTestExt() {
 /**
  * Hides/Unhides the textbox for the abook-path according to whether the thunderbird radio button is selected or not.
  */
-function onChangeFormatSelection() {
-	if (document.getElementsByAttribute('id', 'calendar-format-local')[0].selectedItem.value == 'default') {
-		document.getElementsByAttribute('id', 'abook-lbl')[0].setAttribute('hidden','true');
-		document.getElementsByAttribute('id', 'abook-uri-local')[0].setAttribute('hidden','true');
+function cTBD_onChangeFormatSelection() {
+	if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value == 'default') {
+		document.getElementsByAttribute('id', 'cTBD-abook-lbl')[0].setAttribute('hidden','true');
+		document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].setAttribute('hidden','true');
 	} else {
-		document.getElementsByAttribute('id', 'abook-lbl')[0].setAttribute('hidden','false');
-		document.getElementsByAttribute('id', 'abook-uri-local')[0].setAttribute('hidden','false');
+		document.getElementsByAttribute('id', 'cTBD-abook-lbl')[0].setAttribute('hidden','false');
+		document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].setAttribute('hidden','false');
 	}
 	
-	checkRequiredLocal();
+	cBD_checkRequiredLocal();
 }
 
 /**
  * Checks if either default format is selected or the abook-path contains /abook.mab and enables forwarding if so.
  */
-function checkRequiredLocal() {
+function cBD_checkRequiredLocal() {
 	var canAdvance = false;
 	
-	if (document.getElementsByAttribute('id', 'calendar-format-local')[0].selectedItem.value == 'default' ||
-			document.getElementsByAttribute('id', 'abook-uri-local')[0].value.match(/.*[\/\\]abook.mab$/i))
+	if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value == 'default' ||
+			document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].value.match(/.*[\/\\]abook.mab$/i))
 		canAdvance = true;
 	
 	document.getElementById('calendar-wizard').canAdvance = canAdvance;
@@ -58,9 +58,9 @@ function checkRequiredLocal() {
 /**
  * Fills the textbox on the customizePage with a proposition, if thunderbird has been choosen as calender format
  */
-function initNameLocal() {
+function cTBD_initName() {
     var nameField = document.getElementById("calendar-name");
-    if (document.getElementsByAttribute('id', 'calendar-format-local')[0].selectedItem.value != 'thunderbird'
+    if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value != 'thunderbirthday'
 				|| nameField.value)
         return;
 
@@ -68,9 +68,11 @@ function initNameLocal() {
 }
 
 /**
- * Sets calender-type and calender-format to thunderbird, so the appropriate provider will be choosen on calendar creation
+ * Sets calender-type and calender-format to thunderbirthday, so the appropriate provider will be choosen on calendar creation
  */
-function preCreateCalendar() {
-	document.getElementById('calendar-type').selectedItem.value = "thunderbird";
-	document.getElementById('calendar-format').selectedItem.value = "thunderbird";
+function cTBD_preCreateCalendar() {
+	if (document.getElementById('cTBD-calendar-format').selectedItem.value == "thunderbirthday") {
+		document.getElementById('calendar-type').selectedItem.value = "thunderbirthday";
+		document.getElementById('calendar-format').selectedItem.value = "thunderbirthday";
+	}
 }
