ff18e358825f5cefb61bdbf705ea497164cc1e26
ingo <ingo@c6a9ee40-8b02-4321-a5a2-18dd8e6a8d40>
2008-12-22 21:22:14 +0000

    IMPORT: dialog extended + basic provider ready.
    git-svn-id: file:///tmp/tbd-svn-repo/trunk@4 c6a9ee40-8b02-4321-a5a2-18dd8e6a8d40

--- chrome/content/calendarCreation.js
+++ chrome/content/calendarCreation.js
@@ -13,7 +13,45 @@ function cTBD_loadCalendarCreation() {
 
 	customizePage.setAttribute('onpageadvanced',
 				 "cTBD_preCreateCalendar(); " + customizePage.getAttribute('onpageadvanced'));
+	
+	// Fill Dropdownbox with adressbooks
+	cTBD_fillDropDownBox();
+	
+	// debug
+	test();
 }
+
+
+/**
+ * Fills the dropdownbox on the cTBD_locationPageLocal page with the names of the adresbooks
+ */
+function cTBD_fillDropDownBox() {
+	var abRdf = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
+	var abRootDir = abRdf.GetResource("moz-abdirectory://").QueryInterface(Components.interfaces.nsIAbDirectory);
+	
+	var abSubDirs = abRootDir.childNodes.QueryInterface(Components.interfaces.nsISimpleEnumerator);
+	
+	var listbox = document.getElementsByAttribute('id', 'cTBD-abook-uri-popup')[0];
+	
+	// "All adressbooks" item
+	var menuitem = document.createElement("menuitem");
+	menuitem.setAttribute('label',"Alle Adressbuecher");
+	menuitem.setAttribute('value',"moz-abdirectory://");
+	
+	listbox.appendChild(menuitem);
+	
+	// List of adressbooks
+	while (abSubDirs.hasMoreElements()) {
+		var abDir = abSubDirs.getNext().QueryInterface(Components.interfaces.nsIAbDirectory);
+		
+		var menuitem = document.createElement("menuitem");
+		menuitem.setAttribute('label',abDir.dirName);
+		menuitem.setAttribute('value',abDir.directoryProperties.URI);
+		
+		listbox.appendChild(menuitem);
+	}
+}
+
 
 /**
  * Replaces the onInitialAdvance() function. Changes the next page after the initial page according to the selected radio button.
@@ -31,26 +69,25 @@ function cTBD_onInitialAdvance() {
  * Hides/Unhides the textbox for the abook-path according to whether the thunderbird radio button is selected or not.
  */
 function cTBD_onChangeFormatSelection() {
-	if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value == 'default') {
-		document.getElementsByAttribute('id', 'cTBD-abook-lbl')[0].setAttribute('hidden','true');
-		document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].setAttribute('hidden','true');
-	} else {
-		document.getElementsByAttribute('id', 'cTBD-abook-lbl')[0].setAttribute('hidden','false');
+	if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value == 'thunderbirthday') {
 		document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].setAttribute('hidden','false');
+	} else {
+		document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].setAttribute('hidden','true');
 	}
 	
-	cBD_checkRequiredLocal();
+	cTBD_checkRequiredLocal();
 }
 
 /**
- * Checks if either default format is selected or the abook-path contains /abook.mab and enables forwarding if so.
+ * Checks if either default format is selected or cTBD-abook-uri is a valid uri ending with /abook.mab and enables forwarding if so.
  */
-function cBD_checkRequiredLocal() {
+function cTBD_checkRequiredLocal() {
 	var canAdvance = false;
 	
-	if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value == 'default' ||
-			document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].value.match(/.*[\/\\]abook.mab$/i))
+	if (document.getElementsByAttribute('id', 'cTBD-calendar-format')[0].selectedItem.value != 'thunderbirthday' ||
+				document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].selectedItem != null) {
 		canAdvance = true;
+	}
 	
 	document.getElementById('calendar-wizard').canAdvance = canAdvance;
 }
@@ -72,7 +109,60 @@ function cTBD_initName() {
  */
 function cTBD_preCreateCalendar() {
 	if (document.getElementById('cTBD-calendar-format').selectedItem.value == "thunderbirthday") {
+		document.getElementById("calendar-uri").value = 
+					document.getElementsByAttribute('id', 'cTBD-abook-uri')[0].value;
 		document.getElementById('calendar-type').selectedItem.value = "thunderbirthday";
 		document.getElementById('calendar-format').selectedItem.value = "thunderbirthday";
+	}
+}
+
+/**
+ * Takes a string and returns an nsIURI
+ *
+ * @param aUriString  the string of the address to for the spec of the nsIURI
+ *
+ * @returns  an nsIURI whose spec is aUriString
+ */
+function makeURL(aUriString) {
+    var ioSvc = Cc["@mozilla.org/network/io-service;1"].
+                getService(Ci.nsIIOService);
+    return ioSvc.newURI(aUriString, null, null);
+}
+
+function test() {
+	// return;
+	
+	var abRdf = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
+	var abRootDir = abRdf.GetResource("moz-abdirectory://").QueryInterface(Components.interfaces.nsIAbDirectory);
+	
+	with (abRootDir.directoryProperties) alert (abRootDir.dirName + dirType + fileName + URI);
+	
+	var abSubDirs = abRootDir.childNodes.QueryInterface(Components.interfaces.nsISimpleEnumerator);
+	// var abSubDirs = abRootDir.QueryInterface(Components.interfaces.nsISimpleEnumerator);
+	
+	while (abSubDirs.hasMoreElements()) {
+		var abDir = abSubDirs.getNext().QueryInterface(Components.interfaces.nsIAbDirectory);
+		
+		// alert (abDir.dirName);
+		// with (abDir.directoryProperties) alert (dirType + fileName + URI);
+		
+		// alert(abDir.childCards);
+		
+		var abCardsEnum = abDir.childCards.QueryInterface(Components.interfaces.nsIEnumerator);
+		try {
+			abCardsEnum.first();
+			do {
+				var abCard = abCardsEnum.currentItem().QueryInterface(Components.interfaces.nsIAbCard);
+				
+				with (abCard) alert("ab: "
+							+ getCardValue("BirthYear")
+							+ getCardValue("BirthMonth") 
+							+ getCardValue("BirthDay"));
+				
+				// abCardsEnum.next();
+			} while(abCardsEnum.next())
+		} catch (e) {
+			alert(e.message);
+		}
 	}
 }
